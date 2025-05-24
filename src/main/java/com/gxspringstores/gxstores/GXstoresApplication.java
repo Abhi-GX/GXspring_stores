38cd82bc7a0a143c0ea8d6cdc8f4c231d0b96d81
abhilash <abhigxtheupm@gmail.com>
2025-05-24 21:25:16 +0530

    title page

--- src/main/java/com/gxspringstores/gxstores/GXstoresApplication.java
+++ src/main/java/com/gxspringstores/gxstores/GXstoresApplication.java
@@ -2,7 +2,6 @@ package com.gxspringstores.gxstores;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class GXstoresApplication {
 
