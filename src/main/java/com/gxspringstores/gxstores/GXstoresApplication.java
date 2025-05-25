b491f87b84800dd6f7dee7abe59d4cf248030166
abhilash <abhigxtheupm@gmail.com>
2025-05-25 23:40:10 +0530

    emo changes

--- src/main/java/com/gxspringstores/gxstores/GXstoresApplication.java
+++ src/main/java/com/gxspringstores/gxstores/GXstoresApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GXstoresApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(GXstoresApplication.class, args);
+
     }
 
 }
